a87ee2d222abf7562ae88ad5b0e23b0bbca1c163
dziakola <magdalena.dziakola@gmail.com>
2023-02-15 14:16:59 +0100

    hidden class filter

--- src/js/script.js
+++ src/js/script.js
@@ -17,35 +17,79 @@ render(bookTemplate,dataSrc,booksList);
 
 const books = document.querySelector('.books-list');
 const filterForm = document.querySelector('.filters');
+const booksImage = document.querySelectorAll('.book__image');
+const dataBooks = dataSource.books;
+const bookImageClass = 'book__image';
+const favoriteClass = 'favorite';
+const hiddenClass = 'hidden';
+
+
+
+function filterBooks(dataList,filter){
+  let shouldBeHidden = false;
+  for(let item of dataList){
+    //obiekt: id,name,image...
+    for(let filtr of filter){
+      //[adults,fiction]
+      if(!filtr) shouldBeHidden=false;
+      for(let book of booksImage){
+        //obiekt dom z books__image
+        
+        if(item.details.hasOwnProperty(filtr) && (book.dataset.id == item.id)){
+          //obiekt.details=='fiction' i book_image.id=obiekt.id
+          if(item.details[filtr]){ 
+            //obiekt.details=='fiction'==true
+            //book.classList.remove(hiddenClass);
+            shouldBeHidden = false;
+            console.log(book.classList);
+          }else if(!item.details[filtr]){
+            //obiekt.details=='fiction'==false
+            //book.classList.add(hiddenClass);
+            shouldBeHidden = true;
+            console.log(book.classList);
+          }
+          if(shouldBeHidden) book.classList.add(hiddenClass);
+        }
+        
+      }
+      
+    }
+
+      
+  }
+  
+
+}
 function initActions(list, form){
   const filters = [];
   const tab = [];
   //dwukrotne kliknięcie na książke powoduje dodanie klasy favorite do .book__image 
   list.addEventListener('dblclick', function(e){
     e.preventDefault();
-    console.log(e.target.offsetParent);
-    if((e.target.offsetParent.classList.contains('book__image'))){
-      e.target.offsetParent.classList.toggle('favorite');
+    if((e.target.offsetParent.classList.contains(bookImageClass))){
+      e.target.offsetParent.classList.toggle(favoriteClass);
       //dwukrotne kliknięcie powoduje dodanie książki do tablicy favoriteBooks
-      if(e.target.offsetParent.classList.contains('favorite')){
+      if(e.target.offsetParent.classList.contains(favoriteClass)){
         tab.push(e.target.offsetParent.dataset.id);
-        console.log(tab);
       }else{
         tab.splice(tab.indexOf(e.target.offsetParent.dataset.id),1);
-        console.log(tab);
       }
     }
   });
+  
   form.addEventListener('click', function(e){
     if(e.target.tagName == 'INPUT' && e.target.type == 'checkbox' && e.target.name == 'filter'){
+      
+      
       if(e.target.checked){
         filters.push(e.target.value);
-        console.log(filters);
       }else if(!e.target.checked){
         filters.splice((filters.indexOf(e.target.value)),1);
-        console.log(filters);
       }
+      
+      filterBooks(dataBooks, filters);
     }
+    
   });
 }
 
